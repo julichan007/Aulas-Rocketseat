8615be4351cb71d7e9cab399b592e8fc379c08e1
Juliana Barreto <julichan.sousa@gmail.com>
2020-03-11 15:58:06 -0300

    Commit Inicial

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Header from './componentes/Header';
+
+const App = () => (
+    <div className="App">
+      <Header/>
+    </div>
+);
+
+
+export default App;
